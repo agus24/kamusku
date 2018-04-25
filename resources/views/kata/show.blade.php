76b487d9e0c4c6d95e43f58de97fb231333308cb
agus24 <agusx244@gmail.com>
2018-04-25 20:18:56 +0700

    fix show kata title

--- resources/views/kata/show.blade.php
+++ resources/views/kata/show.blade.php
@@ -4,18 +4,18 @@
     <div class="row justify-content-center">
         <div class="col-md-10 col-md-offset-1">
             <div class="card">
-                <div class="card-header">asdfasdf</div>
+                <div class="card-header">Daftar Kata Bahasa {{ $kata[0]->bahasa->nama }}</div>
                 <div class="card-body">
                     <div class="col-md-2 float-right">
                         <form action="" method="GET">
                             <input type="text" class="form-control" name="search" placeholder="search" value="{{ $_GET['search'] ?? "" }}">
                         </form><br>
                     </div>
-                    <table class="table table-bordered table-stripped">
+                    <table class="table table-responsive">
                         <thead>
-                            <th>Kata</th>
-                            <th>Jumlah Translate</th>
-                            <th>Action</th>
+                            <th width="40%">Kata</th>
+                            <th width="10%">Jumlah Translate</th>
+                            <th width="50%">Action</th>
                         </thead>
                         @foreach($kata as $value)
                             <tr>
